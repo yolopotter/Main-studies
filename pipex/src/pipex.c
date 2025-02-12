bbb725e7e16ead8996e2718e826b84e78474a7d4
Vladimir Lopatinski <vlopatin@c1r3p4.hive.fi>
2025-02-12 11:20:29 +0200

    stashing

--- pipex/src/pipex.c
+++ pipex/src/pipex.c
@@ -6,7 +6,7 @@
 /*   By: vlopatin <vlopatin@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/04 10:44:10 by vlopatin          #+#    #+#             */
-/*   Updated: 2025/02/11 17:07:30 by vlopatin         ###   ########.fr       */
+/*   Updated: 2025/02/12 11:19:30 by vlopatin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ void	child_process1(t_side *left, t_side *right, int *pipe_fd, char **envp)
 	{
 		close_fds(pipe_fd[0], NULL, NULL, 0);
 		close_fds(right->fd, NULL, NULL, 0);
-		dup2(left->fd, STDIN_FILENO);
+		// dup2(left->fd, STDIN_FILENO);
 		dup2(pipe_fd[1], STDOUT_FILENO);
 		close_fds(left->fd, NULL, NULL, 0);
 		close_fds(pipe_fd[1], NULL, NULL, 0);
-		if (execve(left->path, left->cmd, envp) == -1)
-			exit_error(6, &(left->cmd), &(left->path), EXECVE);
+		execve(left->path, left->cmd, envp);
+			// exit_error(6, &(left->cmd), &(left->path), EXECVE);
 	}
 }
 
@@ -43,8 +43,8 @@ void	child_process2(t_side *left, t_side *right, int *pipe_fd, char **envp)
 		dup2(right->fd, STDOUT_FILENO);
 		close_fds(pipe_fd[0], NULL, NULL, 0);
 		close_fds(right->fd, NULL, NULL, 0);
-		if (execve(right->path, right->cmd, envp) == -1)
-			exit_error(6, &(right->cmd), &(right->path), EXECVE);
+		execve(right->path, right->cmd, envp);
+			// exit_error(6, &(right->cmd), &(right->path), EXECVE);
 	}
 }
 
@@ -63,6 +63,11 @@ void	parent_process(t_side *left, t_side *right, int *pipe_fd)
 //to fix: make child1 run even if path is not found OR file open fails. Only dont run execve when "".
 //Any other input into execve should be fine. perror will print correct error after fail inside execve
 
+//Issue currently:
+// % ./pipex file5 "cat" "sad" file9
+//pipex: Command not found: sad
+
+
 int	main(int ac, char **av, char **envp)
 {
 	int		pipe_fd[2];
@@ -73,12 +78,17 @@ int	main(int ac, char **av, char **envp)
 		exit_error(1, NULL, NULL, AC2);
 	init_all(&left, &right, pipe_fd);
 	init_left_side(&left, av, envp);
-	init_right_side(&left, &right, av, envp);
 	if (pipe(pipe_fd) == -1)
 		pipe_fail(&left, &right);
+	// printf("left.is_valid %i\n", left.is_valid);
 	if (left.is_valid)
+	{
 		child_process1(&left, &right, pipe_fd, envp);
-	waitpid(left.pid, NULL, 0);
+		waitpid(left.pid, NULL, WNOHANG);
+	}
+	else
+		dup2(left.fd, STDIN_FILENO);
+	init_right_side(&left, &right, av, envp);
 	child_process2(&left, &right, pipe_fd, envp);
 	parent_process(&left, &right, pipe_fd);
 	return (0);
